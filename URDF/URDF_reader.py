fcbc5eabe585acd1eb3b28c828fdad4416e7c85b
R Hunter <rh1937@elclas23075.its.york.ac.uk>
2024-03-13 09:34:14 +0000

    Initial commit

---
+++ URDF/URDF_reader.py
@@ -0,0 +1,19 @@
+import pybullet as p
+import time
+import pybullet_data
+
+if __name__ == '__main__':
+    physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+    p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+    p.setGravity(0, 0, 0)
+    #planeId = p.loadURDF("plane.urdf")
+    ObjId = p.loadURDF("/home/userfs/r/rh1937/Kinematic_Assignment/URDF/tutorial.urdf")
+    # You can also specify the position and orientation by
+    #------------------------------------------------------------------------
+    # startPos = [0,0,1]
+    # startOrientation = p.getQuaternionFromEuler([0,0,0])
+    # boxId = p.loadURDF("example.urdf",startPos, startOrientation)
+    #------------------------------------------------------------------------
+    while True:
+        p.stepSimulation()
+        time.sleep(1./240.)
