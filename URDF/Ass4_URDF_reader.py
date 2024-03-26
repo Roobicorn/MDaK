2ad133677c1908c70a9bb8c8617e3527a31d305a
R Hunter <rh1937@elclas23075.its.york.ac.uk>
2024-03-26 14:23:18 +0000

    complete ass1 model, begin ass4 file

---
+++ URDF/Ass4_URDF_reader.py
@@ -0,0 +1,22 @@
+import pybullet as p
+import time
+import pybullet_data
+
+if __name__ == '__main__':
+    physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+    p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+    p.setGravity(0, 0, -9.81)
+    planeId = p.loadURDF("plane.urdf")
+    #Load Table & Balls URDFs
+
+    ObjId = p.loadURDF("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/MDaK_assignment_4.urdf", [0,0,0])
+    # You can also specify the position and orientation by
+    #------------------------------------------------------------------------
+    # startPos = [0,0,1]
+    # startOrientation = p.getQuaternionFromEuler([0,0,0])
+    # boxId = p.loadURDF("example.urdf",startPos, startOrientation)
+    #------------------------------------------------------------------------
+
+    while True:
+        p.stepSimulation()
+        time.sleep(1./240.)
