477e876c5fb0f0961d16c2003e6a8cff378c9d8d
R Hunter <rh1937@elclas23074.its.york.ac.uk>
2024-04-02 16:58:42 +0100

    trying xacro and pybullet commands

--- URDF/Ass1_URDF_reader.py
+++ URDF/Ass1_URDF_reader.py
@@ -7,13 +7,15 @@ if __name__ == '__main__':
     p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
     p.setGravity(0, 0, -9.81)
     planeId = p.loadURDF("plane.urdf")
-    ObjId = p.loadURDF("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/MDaK_assignment_1.urdf", [0,0,0])
+    fr3_robot = p.loadURDF("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/MDaK_assignment_1.urdf", [0,0,0])
     # You can also specify the position and orientation by
     #------------------------------------------------------------------------
     # startPos = [0,0,1]
     # startOrientation = p.getQuaternionFromEuler([0,0,0])
     # boxId = p.loadURDF("example.urdf",startPos, startOrientation)
     #------------------------------------------------------------------------
+
+    ball = p.createVisualShape(p.GEOM_SPHERE, 0.5)
 
     while True:
         p.stepSimulation()
