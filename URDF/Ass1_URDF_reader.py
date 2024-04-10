0c35f7e55bba673fdef44dd7ab49d545cdb4983b
R Hunter <rh1937@elclas23075.its.york.ac.uk>
2024-04-10 11:00:40 +0100

    add Path object to reader.py

--- URDF/Ass1_URDF_reader.py
+++ URDF/Ass1_URDF_reader.py
@@ -1,13 +1,18 @@
 import pybullet as p
 import time
 import pybullet_data
+from pathlib import Path
+
+#Change project folder file path to run on local machine
+project_folder = Path("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/")
+fr3_urdf_file = project_folder / "MDaK_assignment_1.urdf"
 
 if __name__ == '__main__':
     physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
     p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
     p.setGravity(0, 0, -9.81)
     planeId = p.loadURDF("plane.urdf")
-    fr3_robot = p.loadURDF("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/MDaK_assignment_1.urdf", [0,0,0])
+    fr3_robot = p.loadURDF(str(fr3_urdf_file.absolute()), [0,0,0])
     # You can also specify the position and orientation by
     #------------------------------------------------------------------------
     # startPos = [0,0,1]
@@ -15,8 +20,7 @@ if __name__ == '__main__':
     # boxId = p.loadURDF("example.urdf",startPos, startOrientation)
     #------------------------------------------------------------------------
 
-    ball = p.createVisualShape(p.GEOM_SPHERE, 0.5)
-
+    
     while True:
         p.stepSimulation()
         time.sleep(1./240.)
--- URDF/Ass4_URDF_reader.py
+++ URDF/Ass4_URDF_reader.py
@@ -2,6 +2,11 @@ import pybullet as p
 import time
 import pybullet_data
 import subprocess
+from pathlib import Path
+
+#Change project folder file path to run on local machine
+project_folder = Path("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/")
+pool_bot_file = project_folder / "pool_bot.urdf"
 
 if __name__ == '__main__':
     physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
@@ -16,7 +21,7 @@ if __name__ == '__main__':
     # urdf_file = "/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/pool_bot.urdf"
     # subprocess.run(['xacro', xacro_file, '>', urdf_file])
 
-    ObjId = p.loadURDF("/home/userfs/r/rh1937/Kinematic_Assignment/MDaK/URDF/pool_bot.urdf", [0,0,0])
+    ObjId = p.loadURDF(str(pool_bot_file.absolute()), [0,0,0])
     # You can also specify the position and orientation by
     #------------------------------------------------------------------------
     # startPos = [0,0,1]
