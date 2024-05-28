56d365a77688e5251aab198035da4df8514f91a5
R Hunter <rh1937@elclas23074.its.york.ac.uk>
2024-05-28 16:57:32 +0100

    ass2.1 done, working on FK

--- URDF/Ass1_URDF_reader.py
+++ URDF/Ass1_URDF_reader.py
@@ -56,32 +56,58 @@ if __name__ == '__main__':
               #"locInertialPos =", linkStates[i][2], "locInertialOrn =", p.getEulerFromQuaternion(linkStates[i][3]),
               "worldLinkFramePos=", linkStates[i][4], "worldLinkFrameOrn=", p.getEulerFromQuaternion(linkStates[i][5]))
 
-    #for printing link info in simulation
-    stepcount = 0
-    testLink = 23
 
     ####################################################################################################################
     # Assignment 2.1: Homogeneous Transform Matrices to the base frame for each joint
 
     d_rotation_matrices = {}
     d_position_matrices = {}
-    np.set_printoptions(precision=3, suppress=True)
+    d_homogeneous_t_matrices = {}
+    t_matrix_bottom_row = np.array([0, 0, 0, 1])
+
+    np.set_printoptions(precision=4) #, suppress=True)
 
     for joint in rJointIds:
         print("Transformation matrices for joint", index_to_joint_name.get(joint), "(joint ID:", joint, "):")
-        d_position_matrices[joint] = np.array([[linkStates[joint][0][0]],[linkStates[joint][0][1]],[linkStates[joint][0][2]]])
+        d_position_matrices[joint] = np.array([[linkStates[joint][0][i]] for i in range(3)])
         print("Position Matrix: \n", d_position_matrices.get(joint))
 
+        rotation_list = p.getMatrixFromQuaternion(linkStates[joint][1])
+        d_rotation_matrices[joint] = np.array([[rotation_list[i] for i in range(j*3, (j*3)+3)] for j in range(3)])
+        print("Rotation Matrix: \n", d_rotation_matrices.get(joint))
+
+        d_homogeneous_t_matrices[joint] = \
+            np.vstack((np.concatenate((d_rotation_matrices[joint], d_position_matrices[joint]), axis=1), t_matrix_bottom_row))
+        print("Homogeneous Transformation Matrix: \n", d_homogeneous_t_matrices[joint], "\n")
+
+    ####################################################################################################################
+    # Assignment 2.2: Compute the Forward Kinematics for one configuration of your robot. (default configuration)
+
+    #Apply transformation matrix for joint 'arm_link_7a_to_6a' (Joint ID: 23)
+    print("Forward Kinematics for joint", index_to_joint_name.get(rJointIds[-1]), "(joint ID:", rJointIds[-1], "):")
+    print("v' = Tv")
+
+    formatted_t_matrix = np.array([[f"{item:.3f}" for item in row] for row in d_homogeneous_t_matrices[rJointIds[-1]]])
+
+    print("[[x']     ", formatted_t_matrix[0,:], " [[x]\n",
+          " [y']  =  ", formatted_t_matrix[1,:],"  [y]\n",
+          " [z']]    ", formatted_t_matrix[2,:], "  [z]]\n", sep="")
+
+    local_coords = np.array([[0],[0], [0], [1]])
+    print("local coordinates = \n", local_coords)
+    base_frame_coords = np.dot(d_homogeneous_t_matrices, local_coords) #or matmul?
+    print("local coordinates = \n", base_frame_coords)
 
 
 
+
+
+    # #for printing link info in simulation
+    # stepcount = 0
+    # testLink = 23
 
     while True:
         p.stepSimulation()
-
-
-
-
 
 # For forward kinematics
         # stepcount += 1
